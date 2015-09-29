89bd040d45f74de266afcb9ef61e118cc7b6922b
Evgeniya Pronina <rezinkina@list.ru>
2015-09-29 18:57:11 +0600

    auth + portfolio + contacts ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,37 @@
 $(document).ready(function() {
+	
 	console.log("js works");
 	console.log($);
-	$(".lock_box").click(function(){
-		$(this).toggleClass("lock_box_active");
-	});
+	
+	// Переделать эту функцию после того, как будет реализован функционал регистрации.
+	var openCloseAuthPopup = function() {
+		$(".lock_box").click(function() {
+			$(this).toggleClass("lock_box_active");
+			if ($(".authorize_popup").hasClass("hidden")) {
+				$(".authorize_popup").removeClass("hidden");
+				$(".authorize_window").removeClass("hidden");
+				$(".s_header").addClass("unvisible");
+				$(".page_wrap").addClass("unvisible");
+				$(".footer").addClass("unvisible");
+			} else {
+				$(".authorize_popup").addClass("hidden");
+				$(".authorize_window").addClass("hidden");
+				$(".s_header").removeClass("unvisible");
+				$(".page_wrap").removeClass("unvisible");
+				$(".footer").removeClass("unvisible");
+			}
+		});
+
+		$(".authorize_window_close_x").click(function() {
+			$(".lock_box").removeClass("lock_box_active");
+			$(".authorize_popup").addClass("hidden");
+			$(".authorize_window").addClass("hidden");
+			$(".s_header").removeClass("unvisible");
+			$(".page_wrap").removeClass("unvisible");
+			$(".footer").removeClass("unvisible");
+		});
+	}
+
+	openCloseAuthPopup();
+
 });
